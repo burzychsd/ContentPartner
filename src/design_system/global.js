9cbf31aac5b4b76d87e4387b6ee76cafcaf22e11
burzychs <burzychs@gmail.com>
2019-03-03 06:05:05 +0100

    refactor svg animation

--- src/design_system/global.js
+++ src/design_system/global.js
@@ -1,7 +1,31 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, keyframes } from 'styled-components'
 import * as text from './typography'
 import * as colors from './colors'
 import bg from './../images/png/background.png'
+
+const dash = keyframes`
+    0% {
+        stroke-dashoffset: 1200;
+        stroke: #FEFCAD;
+        stroke-opacity: 0;
+        fill: none;
+    }
+
+    25% {
+        stroke-opacity: 1;
+    }
+
+    75% {
+        stroke-dashoffset: 0;
+        stroke: none;
+        stroke-opacity: 0;
+        fill: #463E43;
+    }
+    
+    100% {
+        opacity: 0;
+    }
+`
 
 export const styles = createGlobalStyle`
     * {
@@ -47,4 +71,11 @@ export const styles = createGlobalStyle`
     .gatsby-image-wrapper {
         height: 100%; 
     }
+    .path {
+        stroke-dasharray: 261 263;
+        stroke-dashoffset: 1600;
+        animation: ${dash} 2.2s cubic-bezier(0.86, 0, 0.07, 1) 100ms;
+        transition: opacity 0.02s cubic-bezier(0.895, 0.03, 0.685, 0.22);
+    }
+
 `
