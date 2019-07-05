0535b5c15abf231d798201f8d8e06d0f17b54226
burzychs <burzychs@gmail.com>
2019-07-05 17:41:46 +0200

    refactor (gatsby-config.js)

--- gatsby-config.js
+++ gatsby-config.js
@@ -125,7 +125,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        whitelist: ['___gatsby', 'ScrollbarsCustom', 'ScrollbarsCustom-Wrapper'], // Don't remove this selector
+        whitelist: ['___gatsby', 'ScrollbarsCustom', 'ScrollbarsCustom-Wrapper', 'grecaptcha-badge'], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
