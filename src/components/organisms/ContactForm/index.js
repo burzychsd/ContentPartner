1c9d4e10ae2f6f0941a0f9d671b6ce1252d9bb89
burzychs <burzychs@gmail.com>
2019-07-11 01:28:16 +0200

    move recaptcha component to container (ContactForm)

--- src/components/organisms/ContactForm/index.js
+++ src/components/organisms/ContactForm/index.js
@@ -98,49 +98,47 @@ const ContactForm = props => {
     const errorMessageClasses = { ...tw`text-center text-xs sm:text-sm font-body mb-4 mx-auto text-dark_salmon` }
 
     return (
-        <>
-            <AnimatedFlex reset css={tw`flex-col`} style={formContainerStyle}>
-                {status.error && <p css={errorMessageClasses}>{status.msg}</p>}
-                {status.loading && <hr className='contact_loader contact_loader--active' />}
-                <Flex className='contact_details' reset css={tw`flex-col lg:flex-row justify-center items-center mt-2 mx-auto`}>
-                    <Text css={tw`font-bold text-sm my-2 lg:my-0 mx-2 text-center leading-normal`}>FRANCISZEK BUDZBON CONTENT PARTNER</Text>
-                    <Text css={tw`font-light text-sm my-2 sm:my-0 mx-2 text-center leading-normal`}>NIP: 9532764926</Text>
-                    <Text css={tw`font-light text-sm my-2 sm:my-0 mx-2 text-center leading-normal`}>Tel: 690 865 514</Text>
-                </Flex>
-                <Flex as='form' onSubmit={handleSubmit} noValidate css={tw`w-full flex-col justify-between items-center xs:items-start mt-0 mb-24 px-2`}
-                {...formContainerProps}>
-                    <Flex reset css={tw`w-full flex-col xs:flex-row flex-wrap my-2 xs:justify-center items-center xs:my-0 mx-auto`}>
-                        <Flex reset css={tw`flex-col w-5/6 xs:w-2/5 my-4 xs:mr-6`} style={{ height: 80 }}>
-                            <FormField reset {...inputProps('imię i nazwisko')} css={formItemClasses} />
-                            {errors.name && <p css={errorMessageClasses} style={{ marginTop: '1rem' }}>{errors.name}</p>}
-                        </Flex>
-                        <Flex reset css={tw`flex-col w-5/6 xs:w-2/5 my-4 xs:ml-6`} style={{ height: 80 }}>
-                            <FormField reset {...inputProps('adres e-mail')} css={formItemClasses} />
-                            {errors.email && <p css={errorMessageClasses} style={{ marginTop: '1rem' }}>{errors.email}</p>}
-                        </Flex>
+        <AnimatedFlex reset css={tw`flex-col`} style={formContainerStyle}>
+            {status.error && <p css={errorMessageClasses}>{status.msg}</p>}
+            {status.loading && <hr className='contact_loader contact_loader--active' />}
+            <Flex className='contact_details' reset css={tw`flex-col lg:flex-row justify-center items-center mt-2 mx-auto`}>
+                <Text css={tw`font-bold text-sm my-2 lg:my-0 mx-2 text-center leading-normal`}>FRANCISZEK BUDZBON CONTENT PARTNER</Text>
+                <Text css={tw`font-light text-sm my-2 sm:my-0 mx-2 text-center leading-normal`}>NIP: 9532764926</Text>
+                <Text css={tw`font-light text-sm my-2 sm:my-0 mx-2 text-center leading-normal`}>Tel: 690 865 514</Text>
+            </Flex>
+            <Flex as='form' onSubmit={handleSubmit} noValidate css={tw`w-full flex-col justify-between items-center xs:items-start mt-0 mb-24 px-2`}
+            {...formContainerProps}>
+                <Flex reset css={tw`w-full flex-col xs:flex-row flex-wrap my-2 xs:justify-center items-center xs:my-0 mx-auto`}>
+                    <Flex reset css={tw`flex-col w-5/6 xs:w-2/5 my-4 xs:mr-6`} style={{ height: 80 }}>
+                        <FormField reset {...inputProps('imię i nazwisko')} css={formItemClasses} />
+                        {errors.name && <p css={errorMessageClasses} style={{ marginTop: '1rem' }}>{errors.name}</p>}
                     </Flex>
-                    <FormField reset {...inputProps('temat')} css={formItemClasses} />
-                    {errors.topic && <p css={errorMessageClasses}>{errors.topic}</p>}
-                    <FormField 
-                    reset
-                    as='textarea'
-                    name='message'
-                    placeholder='Wiadomość...'
-                    required
-                    style={{ resize: 'none', width: '100%', minHeight: 120, minWidth: '100%',
-                    border: errors.message ? '4px solid #EF946C' : '4px solid #B4EBCA', borderTop: 'none', padding: '1rem', margin: '0 0 1.5rem 0' }}
-                    css={formItemClasses}
-                    onChange={handleChange}
-                    value={values.message || ''} />
-                    {errors.message && <p css={errorMessageClasses}>{errors.message}</p>}
-                    <Flex reset css={tw`w-full wrap justify-between items-center`}>
-                        <Button {...submitButtonProps} css={tw`font-body font-light text-dark_puce`}>Wyślij</Button>
-                        <SocialMedia />
+                    <Flex reset css={tw`flex-col w-5/6 xs:w-2/5 my-4 xs:ml-6`} style={{ height: 80 }}>
+                        <FormField reset {...inputProps('adres e-mail')} css={formItemClasses} />
+                        {errors.email && <p css={errorMessageClasses} style={{ marginTop: '1rem' }}>{errors.email}</p>}
                     </Flex>
                 </Flex>
-            </AnimatedFlex>
+                <FormField reset {...inputProps('temat')} css={formItemClasses} />
+                {errors.topic && <p css={errorMessageClasses}>{errors.topic}</p>}
+                <FormField 
+                reset
+                as='textarea'
+                name='message'
+                placeholder='Wiadomość...'
+                required
+                style={{ resize: 'none', width: '100%', minHeight: 120, minWidth: '100%',
+                border: errors.message ? '4px solid #EF946C' : '4px solid #B4EBCA', borderTop: 'none', padding: '1rem', margin: '0 0 1.5rem 0' }}
+                css={formItemClasses}
+                onChange={handleChange}
+                value={values.message || ''} />
+                {errors.message && <p css={errorMessageClasses}>{errors.message}</p>}
+                <Flex reset css={tw`w-full wrap justify-between items-center`}>
+                    <Button {...submitButtonProps} css={tw`font-body font-light text-dark_puce`}>Wyślij</Button>
+                    <SocialMedia />
+                </Flex>
+            </Flex>
             <Recaptcha action='contact' sitekey='6LeSQKwUAAAAAKFAv8YR1dV3WYJdfVuJdYjqGOFm' onToken={getToken} />
-        </>
+        </AnimatedFlex>
     )
 }
 
